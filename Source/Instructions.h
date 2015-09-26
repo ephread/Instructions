cade7dadc1c241d1e4505371deecb371c29e975b
Frédéric Maquin <maquin.frederic@gmail.com>
2015-09-26 14:54:43 +0200

    First commit - workspace setup

---
+++ Source/Instructions.h
@@ -0,0 +1,19 @@
+//
+//  Instructions.h
+//  Instructions
+//
+//  Created by Frédéric Maquin on 26/09/15.
+//  Copyright © 2015 Ephread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Instructions.
+FOUNDATION_EXPORT double InstructionsVersionNumber;
+
+//! Project version string for Instructions.
+FOUNDATION_EXPORT const unsigned char InstructionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Instructions/PublicHeader.h>
+
+

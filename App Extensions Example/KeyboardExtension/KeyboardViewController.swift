80f0477c88e0123f1de4765f472979ad56598417
Frédéric Maquin <maquin.frederic@gmail.com>
2016-09-06 12:03:18 +0200

    Update license in certain files + update example

--- App Extensions Example/KeyboardExtension/KeyboardViewController.swift
+++ App Extensions Example/KeyboardExtension/KeyboardViewController.swift
@@ -1,10 +1,24 @@
+// KeyboardViewController.swift
 //
-//  KeyboardViewController.swift
-//  Keyboard
+// Copyright (c) 2016 Frédéric Maquin <fred@ephread.com>
 //
-//  Created by Frédéric Maquin on 15/02/16.
-//  Copyright © 2016 Ephread. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 import UIKit
 import InstructionsAppExtensions // <-- If you're using Carthage or managing frameworks manually.
@@ -63,12 +77,12 @@ class KeyboardViewController: UIInputViewController, CoachMarksControllerDataSou
         return 2;
     }
 
-    func coachMarksController(coachMarksController: CoachMarksController, coachMarksForIndex index: Int) -> CoachMark {
+    func coachMarksController(coachMarksController: CoachMarksController, coachMarkForIndex index: Int) -> CoachMark {
         switch(index) {
         case 0:
-            return coachMarksController.coachMarkForView(self.nextKeyboardButton)
+            return coachMarksController.helper.coachMarkForView(self.nextKeyboardButton)
         case 1:
-            return coachMarksController.coachMarkForView(self.secondButton)
+            return coachMarksController.helper.coachMarkForView(self.secondButton)
         default:
             return CoachMark()
         }
@@ -77,7 +91,7 @@ class KeyboardViewController: UIInputViewController, CoachMarksControllerDataSou
 
     func coachMarksController(coachMarksController: CoachMarksController, coachMarkViewsForIndex index: Int, coachMark: CoachMark) -> (bodyView: CoachMarkBodyView, arrowView: CoachMarkArrowView?) {
 
-        let coachViews = coachMarksController.defaultCoachViewsWithArrow(true, arrowOrientation: coachMark.arrowOrientation)
+        let coachViews = coachMarksController.helper.defaultCoachViewsWithArrow(true, arrowOrientation: coachMark.arrowOrientation)
 
         switch(index) {
         case 0:

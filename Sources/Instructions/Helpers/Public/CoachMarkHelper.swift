f70517e478360fa9d2c21b39ee83a740c673b680
klinkert0728 <dklinker@eafit.edu.co>
2018-02-21 10:46:01 -0500

    clean up image extension

--- Sources/Instructions/Helpers/Public/CoachMarkHelper.swift
+++ Sources/Instructions/Helpers/Public/CoachMarkHelper.swift
@@ -188,31 +188,4 @@ public class CoachMarkHelper {
         return  CoachMarkArrowDefaultView(orientation: arrowOrientation!,withCustomColor:color)
     }
 }
-
-extension UIImage {
-func tint(with color: UIColor) -> UIImage
-{
-    UIGraphicsBeginImageContext(self.size)
-    guard let context = UIGraphicsGetCurrentContext() else { return self }
-    
-    // flip the image
-    context.scaleBy(x: 1.0, y: -1.0)
-    context.translateBy(x: 0.0, y: -self.size.height)
-    
-    // multiply blend mode
-    context.setBlendMode(.multiply)
-    
-    let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
-    context.clip(to: rect, mask: self.cgImage!)
-    color.setFill()
-    context.fill(rect)
-    
-    // create UIImage
-    guard let newImage = UIGraphicsGetImageFromCurrentImageContext() else { return self }
-    UIGraphicsEndImageContext()
-    
-    return newImage
-}
-
-}
 public typealias CutoutPathMaker = (_ frame: CGRect) -> UIBezierPath

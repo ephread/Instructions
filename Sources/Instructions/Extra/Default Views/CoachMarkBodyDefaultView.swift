3edabd1d764a217c86386327540ad1b770f09274
liuliyang <bjliuliyang@corp.netease.com>
2018-06-06 16:10:20 +0800

    remove backgroundImageView for custom setting body backGround color

--- Sources/Instructions/Extra/Default Views/CoachMarkBodyDefaultView.swift
+++ Sources/Instructions/Extra/Default Views/CoachMarkBodyDefaultView.swift
@@ -46,15 +46,6 @@ public class CoachMarkBodyDefaultView: UIControl, CoachMarkBodyView {
 
     public var nextLabel: UILabel { return views.nextLabel }
     public var hintLabel: UITextView { return views.hintLabel }
-    public var backgroundImageView: UIImageView {
-        set{
-            views.backgroundImageView = newValue
-        }
-        get{
-            return views.backgroundImageView
-        }
-        
-    }
 
     public weak var highlightArrowDelegate: CoachMarkBodyHighlightArrowDelegate?
 

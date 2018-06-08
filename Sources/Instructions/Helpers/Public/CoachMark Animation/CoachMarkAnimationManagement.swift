fe891e2a2357dad3a4c477d1601727e59d2448cd
Frédéric Maquin <maquin.frederic@gmail.com>
2018-06-08 13:37:12 +1000

    Add missing UIKit import

--- Sources/Instructions/Helpers/Public/CoachMark Animation/CoachMarkAnimationManagement.swift
+++ Sources/Instructions/Helpers/Public/CoachMark Animation/CoachMarkAnimationManagement.swift
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import Foundation
+import UIKit
 
 // MARK: Main protocols
 /// Define context in which the transition will execute. Note that here, what "context" means

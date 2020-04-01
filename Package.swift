77980bf17800e8a6a89a59fe220a47459972eccb
Frédéric Maquin <frederic@maquin.email>
2020-04-01 21:12:04 +0200

    Fix typo

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "Instructions",
-    platform: [
-        .ios(.v9)
+    platforms: [
+        .iOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
